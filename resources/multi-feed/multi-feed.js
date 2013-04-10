092589ddb73c4463f94af71d7a7083e75491400c
t.nurrohman77@gmail.com <t.nurrohman77@gmail.com@29ff8f6b-89b8-b5af-d60b-16ed9ab89d9f>
2013-04-10 16:33:44 +0000

    http://www.dte.web.id/2013/04/widget-blogger-untuk-menampilkan-daftar.html?showComment=1365520121110#c4648794751169064992

--- resources/multi-feed/multi-feed.js
+++ resources/multi-feed/multi-feed.js
@@ -1,7 +1,7 @@
 // Multi Feed Loader Script by Taufik Nurrohman
 // URL: http://www.dte.web.id, https://plus.google.com/108949996304093815163/about
 
-var multiFeed_defaults = {
+var mf_defaults = {
 	feedsUri: [
 		{
 			name: "Posting JQuery",
@@ -39,34 +39,38 @@ var multiFeed_defaults = {
 	},
 	onLoadComplete: function() {
 		// console.log('Fully Loaded!');
+	},
+	loadFeed: function(index) {
+		var head = document.getElementsByTagName('head')[0],
+			cont = document.getElementById(this.containerId),
+			script = document.createElement('script');
+			script.type = "text/javascript";
+			script.src = this.feedsUri[index].url + '/feeds/posts/summary' + (this.feedsUri[index].tag ? '/-/' + this.feedsUri[index].tag : '') + '?alt=json-in-script&max-results=' + this.numPost + '&callback=listEntries';
+		head.appendChild(script);
 	}
 };
 
-for (var i in multiFeed_defaults) {
-	multiFeed_defaults[i] = (typeof (multiFeed[i]) !== undefined && typeof (multiFeed[i]) !== 'undefined') ? multiFeed[i] : multiFeed_defaults[i];
+for (var i in mf_defaults) {
+	mf_defaults[i] = (typeof (multiFeed[i]) !== undefined && typeof (multiFeed[i]) !== 'undefined') ? multiFeed[i] : mf_defaults[i];
 }
 
 function listEntries(json) {
-
 	var entry = json.feed.entry,
-		o = multiFeed_defaults,
+		o = mf_defaults,
 		ct = document.getElementById(o.containerId),
+		div = document.createElement('div'),
 		skeleton = "<ul>",
 		total = o.feedsUri.length,
 		title, link, summ, img;
-
 	for (var i = 0; i < o.numPost; i++) {
 		if (i == entry.length) break;
-
 		title = (o.titleLength !== "auto") ? entry[i].title.$t.substring(0, o.titleLength) + (o.titleLength < entry[i].title.$t.length ? '&hellip;' : '') : entry[i].title.$t;
 		summ = ("summary" in entry[i]) ? entry[i].summary.$t.replace(/<br ?\/?>/g, " ").replace(/<.*?>/g, "") : "";
 		summ = (o.summaryLength < summ.length) ? summ.substring(0, o.summaryLength) + '&hellip;' : summ;
 		img = ("media$thumbnail" in entry[i]) ? '<img src="' + entry[i].media$thumbnail.url.replace(/\/s72(\-c)?\//, "/s" + o.thumbSize + "-c/") + '" style="width:' + o.thumbSize + 'px;height:' + o.thumbSize + 'px;">' : '<span class="fake-img" style="width:' + o.thumbSize + 'px;height:' + o.thumbSize + 'px;"></span>';
-		
 		for (var j = 0, jen = entry[i].link.length; j < jen; j++) {
 			link = (entry[i].link[j].rel == 'alternate') ? entry[i].link[j].href : '#';
 		}
-
 		skeleton += '<li><div class="inner"' + (!o.autoHeight ? ' style="height:' + o.thumbSize + 'px;overflow:hidden;"' : '') + '>';
 		skeleton += (o.showThumbnail) ? img : '';
 		skeleton += '<div class="title"><a href="' + link + '"' + (o.newTabLink ? ' target="_blank"' : '') + '>' + title + '</a></div>';
@@ -78,23 +82,13 @@ function listEntries(json) {
 	}
 	skeleton += '</ul>';
 	skeleton += '<div class="more-link"><a href="' + o.feedsUri[o.current].url.replace(/\/$/, "") + '/search/label/' + o.feedsUri[o.current].tag + o.readMore.endParam + '"' + (o.newTabLink ? ' target="_blank"' : '') + '>' + o.readMore.text + '</a></div>';
-	ct.children[o.current].innerHTML += skeleton;
+	div.className = o.listClass;
+	div.innerHTML = '<div class="main-title"><h4>' + o.feedsUri[o.current].name + '</h4></div>' + skeleton;
+	ct.appendChild(div);
 	o.onLoadFeed(o.current);
+	if ((o.current + 1) < total) o.loadFeed(o.current + 1);
 	if ((o.current + 1) == total) o.onLoadComplete();
-
 	o.current++;
-
 }
 
-(function(o) {
-	var head = document.getElementsByTagName('head')[0],
-		cont = document.getElementById(o.containerId);
-	for (var i = 0, len = o.feedsUri.length; i < len; i++) {
-		cont.innerHTML += '<div class="' + o.listClass + '"><div class="main-title"><h4>' + o.feedsUri[i].name + '</h4></div>';
-		var script = document.createElement('script');
-			script.type = "text/javascript";
-			script.src = o.feedsUri[i].url + '/feeds/posts/summary' + (o.feedsUri[i].tag ? '/-/' + o.feedsUri[i].tag : '') + '?alt=json-in-script&max-results=' + o.numPost + '&callback=listEntries';
-		head.appendChild(script);
-	}
-	cont.innerHTML += '<div style="clear:both;"></div>';
-})(multiFeed_defaults);
+mf_defaults.loadFeed(0);
